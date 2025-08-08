8ae628f72e51346d9ce76d1bb2d16a90a734a8db
molnia_grom <mgrom250@gmail.com>
2025-08-08 13:50:21 +0300

    Uncomment 7 homework

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return action.payload === "up"
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : [...state].sort((a, b) => b.name.localeCompare(a.name));
 
-            return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            // need to fix
+            return state.filter(u => u.age > 18)
         }
         default:
             return state
